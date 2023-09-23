be6b07c0da580720818f97678dbf8f9f892147e2
Srinivas Baride <srini@exidio.co>
2023-09-23 19:50:12 +0530

    Added statistics main file

---
+++ database/statistic.go
@@ -0,0 +1,57 @@
+package database
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	StatisticCollectionName = "statistics"
+)
+
+func StatisticFindOne(ctx context.Context, db *mongo.Database, filter bson.M, opts ...*options.FindOneOptions) (bson.M, error) {
+	var v bson.M
+	if err := FindOne(ctx, db.Collection(StatisticCollectionName), filter, &v, opts...); err != nil {
+		return nil, findOneError(err)
+	}
+
+	return v, nil
+}
+
+func StatisticInsertOne(ctx context.Context, db *mongo.Database, v bson.M, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error) {
+	return InsertOne(ctx, db.Collection(StatisticCollectionName), v, opts...)
+}
+
+func StatisticFindOneAndUpdate(ctx context.Context, db *mongo.Database, filter, update bson.M, opts ...*options.FindOneAndUpdateOptions) (bson.M, error) {
+	var v bson.M
+	if err := FindOneAndUpdate(ctx, db.Collection(StatisticCollectionName), filter, update, &v, opts...); err != nil {
+		return nil, findOneAndUpdateError(err)
+	}
+
+	return v, nil
+}
+
+func StatisticFind(ctx context.Context, db *mongo.Database, filter bson.M, opts ...*options.FindOptions) ([]bson.M, error) {
+	var v []bson.M
+	if err := Find(ctx, db.Collection(StatisticCollectionName), filter, &v, opts...); err != nil {
+		return nil, findError(err)
+	}
+
+	return v, nil
+}
+
+func StatisticDeleteMany(ctx context.Context, db *mongo.Database, filter bson.M, opts ...*options.DeleteOptions) error {
+	_, err := DeleteMany(ctx, db.Collection(StatisticCollectionName), filter, opts...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func StatisticIndexesCreateMany(ctx context.Context, db *mongo.Database, models []mongo.IndexModel, opts ...*options.CreateIndexesOptions) ([]string, error) {
+	return IndexesCreateMany(ctx, db.Collection(StatisticCollectionName), models, opts...)
+}
