32e9eb5767026969b03f05b9937addcb7f6ee9a7
Srinivas Baride <srini@exidio.co>
2023-12-08 12:44:30 +0530

    Added soft link command

--- scripts/systemd/create.sh
+++ scripts/systemd/create.sh
@@ -19,6 +19,8 @@ for app_name in "$app_directory"/*; do
   app_name=$(basename "$app_name")
   service_file="$systemd_dir/${app_name}.service"
   modified_template="${app_template//APP_NAME/$app_name}"
+
+  ln -s "${GOPATH}/bin/${app_name}" "/usr/local/bin/${app_name}"
   if echo "$modified_template" > "$service_file"; then
     echo "Created $service_file"
   fi
