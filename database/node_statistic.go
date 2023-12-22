bee6496133e8af98949a84869df96a5cbf73794c
Srinivas Baride <srini@exidio.co>
2023-12-23 00:51:50 +0530

    feat: deleted node_statistic model

--- database/node_statistic.go
+++ database/node_statistic.go
@@ -6,38 +6,36 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-
-	"github.com/sentinel-official/explorer/models"
 )
 
 const (
 	NodeStatisticCollectionName = "node_statistics"
 )
 
-func NodeStatisticFindOne(ctx context.Context, db *mongo.Database, filter bson.M, opts ...*options.FindOneOptions) (*models.NodeStatistic, error) {
-	var v models.NodeStatistic
+func NodeStatisticFindOne(ctx context.Context, db *mongo.Database, filter bson.M, opts ...*options.FindOneOptions) (bson.M, error) {
+	var v bson.M
 	if err := FindOne(ctx, db.Collection(NodeStatisticCollectionName), filter, &v, opts...); err != nil {
 		return nil, findOneError(err)
 	}
 
-	return &v, nil
+	return v, nil
 }
 
-func NodeStatisticInsertOne(ctx context.Context, db *mongo.Database, v *models.NodeStatistic, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error) {
+func NodeStatisticInsertOne(ctx context.Context, db *mongo.Database, v bson.M, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error) {
 	return InsertOne(ctx, db.Collection(NodeStatisticCollectionName), v, opts...)
 }
 
-func NodeStatisticFindOneAndUpdate(ctx context.Context, db *mongo.Database, filter, update bson.M, opts ...*options.FindOneAndUpdateOptions) (*models.NodeStatistic, error) {
-	var v models.NodeStatistic
+func NodeStatisticFindOneAndUpdate(ctx context.Context, db *mongo.Database, filter, update bson.M, opts ...*options.FindOneAndUpdateOptions) (bson.M, error) {
+	var v bson.M
 	if err := FindOneAndUpdate(ctx, db.Collection(NodeStatisticCollectionName), filter, update, &v, opts...); err != nil {
 		return nil, findOneAndUpdateError(err)
 	}
 
-	return &v, nil
+	return v, nil
 }
 
-func NodeStatisticFind(ctx context.Context, db *mongo.Database, filter bson.M, opts ...*options.FindOptions) ([]*models.NodeStatistic, error) {
-	var v []*models.NodeStatistic
+func NodeStatisticFind(ctx context.Context, db *mongo.Database, filter bson.M, opts ...*options.FindOptions) ([]bson.M, error) {
+	var v []bson.M
 	if err := Find(ctx, db.Collection(NodeStatisticCollectionName), filter, &v, opts...); err != nil {
 		return nil, findError(err)
 	}
